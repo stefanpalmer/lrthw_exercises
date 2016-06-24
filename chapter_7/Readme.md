6a04f3ec328d214ecf419a0f8311801a57ec934a
Stefan <palmerstefan@yahoo.com>
2016-06-24 10:51:06 -0600

    Add exercise and Readme.md
    [#120477543]

---
+++ chapter_7/Readme.md
@@ -0,0 +1,3 @@
+##### Difference between _print_ and _puts_
+
+The main difference between _print_ and _puts_ is that the _print_ command executes the output on the same line, while the _puts_ command adds a new line after the output.
---
+++ chapter_7/ex7.rb
@@ -0,0 +1,27 @@
+#prints string "Mary had a little lamb."
+puts "Mary had a little lamb."
+#prints string with additional string 'snow'
+puts "Its fleece was white as #{'snow'}"
+#prints string
+puts "And everywhere that Mary went."
+#prints a borderline with ten dots (dot multiplied by 10)
+puts '.' * 10 # what'd that do?
+# List of 12 end variables with strings each consisting of one letter
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# variables end1-6 are added and printed
+print end1 + end2 + end3 + end4 + end5 + end6
+# variables end7-12 are added and printed, but with the puts command instead of print command
+puts end7 + end8 + end9 + end10 + end11 + end12
